b487f4b1736b25045bc26487358207774b836829
shivom <shivomgoel2@gmail.com>
2021-12-15 20:06:38 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  const myApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Material(
+            child: Center(
+      child: Container(
+        child: Text("Welcome to 8 hours of Flutter"),
+      ),
+    )));
+  }
+}
